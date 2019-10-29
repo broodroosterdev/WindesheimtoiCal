4bca6d74ff93c5771c8fd26e804bd6d4e7593a56
broodroosterdev <31385368+broodroosterdev@users.noreply.github.com>
2019-10-29 10:17:18 +0100

    Added warning of wintertijd bug to ical

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ http.createServer(function(req, res) {
                     uid: appointment.id,
                     start: moment.unix(appointment.starttijd / 1000).subtract(2, 'hours'),
                     end: moment.unix(appointment.eindtijd / 1000).subtract(2, 'hours'),
-                    summary: appointment.commentaar,
+                    summary: "TIJD KLOPT NIET. GEBRUIK WINDESHEIM APP " + appointment.commentaar,
                     location: appointment.lokaal,
                     organizer: appointment.klascode
                 });
